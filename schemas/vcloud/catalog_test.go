3b542d99e55d517372be7bad86863c626261858e
Ivan Porto Carrero <icarrero@vmware.com>
2015-10-12 13:44:37 -0700

    adds some tests

---
+++ schemas/vcloud/catalog_test.go
@@ -0,0 +1,29 @@
+package vcloud
+
+import "testing"
+
+func TestFetchCatalog(t *testing.T) {
+
+}
+
+var publicCatalogXML = `<?xml version="1.0" encoding="UTF-8"?>
+<Catalog xmlns="http://www.vmware.com/vcloud/v1.5" name="Public Catalog" id="urn:vcloud:catalog:d666d38a-fcc3-4fae-9a31-83b928921878" href="https://us-california-1-3.vchs.vmware.com/api/compute/api/catalog/d666d38a-fcc3-4fae-9a31-83b928921878" type="application/vnd.vmware.vcloud.catalog+xml" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.vmware.com/vcloud/v1.5 http://us-california-1-3.vchs.vmware.com/api/compute/api/v1.5/schema/master.xsd">
+    <Link rel="down" href="https://us-california-1-3.vchs.vmware.com/api/compute/api/catalog/d666d38a-fcc3-4fae-9a31-83b928921878/metadata" type="application/vnd.vmware.vcloud.metadata+xml"/>
+    <Link rel="down" href="https://us-california-1-3.vchs.vmware.com/api/compute/api/catalog/d666d38a-fcc3-4fae-9a31-83b928921878/controlAccess/" type="application/vnd.vmware.vcloud.controlAccess+xml"/>
+    <Description>vCHS service catalog</Description>
+    <CatalogItems>
+        <CatalogItem href="https://us-california-1-3.vchs.vmware.com/api/compute/api/catalogItem/00acfc20-afd4-4219-a452-5d3eecfa2b26" id="00acfc20-afd4-4219-a452-5d3eecfa2b26" name="CentOS64-64BIT" type="application/vnd.vmware.vcloud.catalogItem+xml"/>
+        <CatalogItem href="https://us-california-1-3.vchs.vmware.com/api/compute/api/catalogItem/178c60b8-b42a-4c12-bfdd-ff0a2c8bd721" id="178c60b8-b42a-4c12-bfdd-ff0a2c8bd721" name="CentOS64-32BIT" type="application/vnd.vmware.vcloud.catalogItem+xml"/>
+        <CatalogItem href="https://us-california-1-3.vchs.vmware.com/api/compute/api/catalogItem/1b868313-3d5b-4e71-af8e-82f7d54bdd1e" id="1b868313-3d5b-4e71-af8e-82f7d54bdd1e" name="Ubuntu Server 12.04 LTS (amd64 20150127)" type="application/vnd.vmware.vcloud.catalogItem+xml"/>
+        <CatalogItem href="https://us-california-1-3.vchs.vmware.com/api/compute/api/catalogItem/22ba359a-d5a6-4026-9dd5-408277666b49" id="22ba359a-d5a6-4026-9dd5-408277666b49" name="Ubuntu Server 12.04 LTS (i386 20150127)" type="application/vnd.vmware.vcloud.catalogItem+xml"/>
+        <CatalogItem href="https://us-california-1-3.vchs.vmware.com/api/compute/api/catalogItem/5b0f892d-a1d6-467f-ac53-feb9f6e5cc17" id="5b0f892d-a1d6-467f-ac53-feb9f6e5cc17" name="W2K12-STD-64BIT" type="application/vnd.vmware.vcloud.catalogItem+xml"/>
+        <CatalogItem href="https://us-california-1-3.vchs.vmware.com/api/compute/api/catalogItem/63daa159-62bf-460d-8c02-907448eeefdf" id="63daa159-62bf-460d-8c02-907448eeefdf" name="CentOS63-64BIT" type="application/vnd.vmware.vcloud.catalogItem+xml"/>
+        <CatalogItem href="https://us-california-1-3.vchs.vmware.com/api/compute/api/catalogItem/7b1f43ad-a21e-412a-a519-c8d877fe4f92" id="7b1f43ad-a21e-412a-a519-c8d877fe4f92" name="VMware Photon OS - Tech Preview 2" type="application/vnd.vmware.vcloud.catalogItem+xml"/>
+        <CatalogItem href="https://us-california-1-3.vchs.vmware.com/api/compute/api/catalogItem/81b47b60-5721-4aa8-8663-0f08c3592a51" id="81b47b60-5721-4aa8-8663-0f08c3592a51" name="W2K12-STD-R2-64BIT" type="application/vnd.vmware.vcloud.catalogItem+xml"/>
+        <CatalogItem href="https://us-california-1-3.vchs.vmware.com/api/compute/api/catalogItem/c2d74058-7bcf-43f3-868e-8a2dda71182f" id="c2d74058-7bcf-43f3-868e-8a2dda71182f" name="W2K8-STD-R2-64BIT" type="application/vnd.vmware.vcloud.catalogItem+xml"/>
+        <CatalogItem href="https://us-california-1-3.vchs.vmware.com/api/compute/api/catalogItem/f03cdae7-cbf3-4241-bde8-9a91bc6b97e2" id="f03cdae7-cbf3-4241-bde8-9a91bc6b97e2" name="CentOS63-32BIT" type="application/vnd.vmware.vcloud.catalogItem+xml"/>
+    </CatalogItems>
+    <IsPublished>true</IsPublished>
+    <DateCreated>2015-03-31T00:08:51.760Z</DateCreated>
+    <VersionNumber>159</VersionNumber>
+</Catalog>`
---
+++ schemas/vcloud/org_test.go
@@ -0,0 +1,103 @@
+package vcloud
+
+import (
+	"encoding/xml"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/vmware/govcloudair/api"
+)
+
+type testXMLClient struct {
+	XMLName xml.Name `xml:"http://www.vmware.com/vcloud/v1.5 Session"`
+	Links   []*Link  `xml:"Link"`
+	Config  *api.Config
+}
+
+// XMLRequest makes HTTP request that have XML bodies and get XML results
+func (s *testXMLClient) XMLRequest(method, url, tpe string, body, result interface{}) error {
+	return api.XMLRequest(s.Config, method, url, tpe, body, result)
+}
+
+var testConfig = &api.Config{
+	HTTP:  http.DefaultClient,
+	Debug: true,
+}
+
+func TestFetchOrgList(t *testing.T) { // sanity check for serializing the org list
+	serv := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+		rw.Header().Set("Content-Type", MimeOrgList)
+		rw.WriteHeader(200)
+		rw.Write([]byte(orgListXML))
+	}))
+	defer serv.Close()
+
+	fixedSessionXML := strings.Replace(sessionsXML, "https://us-california-1-3.vchs.vmware.com", serv.URL, -1)
+	var tc testXMLClient
+	if err := xml.Unmarshal([]byte(fixedSessionXML), &tc); assert.NoError(t, err) {
+		ol, err := FetchOrgList(tc.Links, &tc)
+		if assert.NoError(t, err) {
+			assert.Len(t, ol.Orgs, 1)
+		}
+	}
+}
+
+func TestFetchOrg(t *testing.T) {
+	//serv := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+	//rw.Header().Set("Content-Type", MimeOrg)
+	//rw.WriteHeader(200)
+	//rw.Write([]byte(orgXML))
+	//}))
+	//defer serv.Close()
+
+	//fixedOrgListXML := strings.Replace(orgListXML, "https://us-california-1-3.vchs.vmware.com", serv.URL, -1)
+	//var tc testXMLClient
+	//if err := xml.Unmarshal([]byte(fixedOrgListXML), &tc); assert.NoError(t, err) {
+	//o, err :=
+	//}
+}
+
+var orgListXML = `<?xml version="1.0" encoding="UTF-8"?>
+<OrgList xmlns="http://www.vmware.com/vcloud/v1.5" href="https://us-california-1-3.vchs.vmware.com/api/compute/api/org/" type="application/vnd.vmware.vcloud.orgList+xml" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.vmware.com/vcloud/v1.5 http://us-california-1-3.vchs.vmware.com/api/compute/api/v1.5/schema/master.xsd">
+    <Org href="https://us-california-1-3.vchs.vmware.com/api/compute/api/org/org-uuid-goes-here" name="org-name-uuid-goes-here" type="application/vnd.vmware.vcloud.org+xml"/>
+</OrgList>
+`
+
+var orgXML = `<?xml version="1.0" encoding="UTF-8"?>
+<Org xmlns="http://www.vmware.com/vcloud/v1.5" name="org-name-uuid-goes-here" id="urn:vcloud:org:org-uuid-goes-here" href="https://us-california-1-3.vchs.vmware.com/api/compute/api/org/org-uuid-goes-here" type="application/vnd.vmware.vcloud.org+xml" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.vmware.com/vcloud/v1.5 http://us-california-1-3.vchs.vmware.com/api/compute/api/v1.5/schema/master.xsd">
+    <Link rel="down" href="https://us-california-1-3.vchs.vmware.com/api/compute/api/vdc/vdc-uuid-goes-here" name="VDC1" type="application/vnd.vmware.vcloud.vdc+xml"/>
+    <Link rel="down" href="https://us-california-1-3.vchs.vmware.com/api/compute/api/tasksList/org-uuid-goes-here" type="application/vnd.vmware.vcloud.tasksList+xml"/>
+    <Link rel="down" href="https://us-california-1-3.vchs.vmware.com/api/compute/api/catalog/default-catalog-uuid-goes-here" name="default-catalog" type="application/vnd.vmware.vcloud.catalog+xml"/>
+    <Link rel="down" href="https://us-california-1-3.vchs.vmware.com/api/compute/api/catalog/default-catalog-uuid-goes-here/controlAccess/" type="application/vnd.vmware.vcloud.controlAccess+xml"/>
+    <Link rel="controlAccess" href="https://us-california-1-3.vchs.vmware.com/api/compute/api/catalog/default-catalog-uuid-goes-here/action/controlAccess" type="application/vnd.vmware.vcloud.controlAccess+xml"/>
+    <Link rel="down" href="https://us-california-1-3.vchs.vmware.com/api/compute/api/catalog/d666d38a-fcc3-4fae-9a31-83b928921878" name="Public Catalog" type="application/vnd.vmware.vcloud.catalog+xml"/>
+    <Link rel="down" href="https://us-california-1-3.vchs.vmware.com/api/compute/api/catalog/d666d38a-fcc3-4fae-9a31-83b928921878/controlAccess/" type="application/vnd.vmware.vcloud.controlAccess+xml"/>
+    <Link rel="add" href="https://us-california-1-3.vchs.vmware.com/api/compute/api/admin/org/org-uuid-goes-here/catalogs" type="application/vnd.vmware.admin.catalog+xml"/>
+    <Link rel="down" href="https://us-california-1-3.vchs.vmware.com/api/compute/api/network/default-routed-network-uuid-goes-here" name="default-routed-network" type="application/vnd.vmware.vcloud.orgNetwork+xml"/>
+    <Link rel="down" href="https://us-california-1-3.vchs.vmware.com/api/compute/api/supportedSystemsInfo/" type="application/vnd.vmware.vcloud.supportedSystemsInfo+xml"/>
+    <Link rel="down" href="https://us-california-1-3.vchs.vmware.com/api/compute/api/org/org-uuid-goes-here/metadata" type="application/vnd.vmware.vcloud.metadata+xml"/>
+    <Link rel="down" href="https://us-california-1-3.vchs.vmware.com/api/compute/api/org/org-uuid-goes-here/hybrid" type="application/vnd.vmware.vcloud.hybridOrg+xml"/>
+    <Link rel="alternate" href="https://us-california-1-3.vchs.vmware.com/api/compute/api/admin/org/org-uuid-goes-here" type="application/vnd.vmware.admin.organization+xml"/>
+    <Link rel="down" href="https://us-california-1-3.vchs.vmware.com/api/compute/api/vdcTemplates" type="application/vnd.vmware.admin.vdcTemplates+xml"/>
+    <Link rel="instantiate" href="https://us-california-1-3.vchs.vmware.com/api/compute/api/org/org-uuid-goes-here/action/instantiate" type="application/vnd.vmware.vcloud.instantiateVdcTemplateParams+xml"/>
+    <Description/>
+    <FullName>full-org-name-uuid-goes-here (org-name-uuid-goes-here)</FullName>
+</Org>
+`
+
+var sessionsXML = `<?xml version="1.0" encoding="UTF-8"?>
+<Session xmlns="http://www.vmware.com/vcloud/v1.5" org="org-name-uuid-goes-here" roles="Account Administrator" user="someone@somewhere.com" userId="urn:vcloud:user:user-uuid-goes-here" href="https://us-california-1-3.vchs.vmware.com/api/compute/api/session" type="application/vnd.vmware.vcloud.session+xml" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.vmware.com/vcloud/v1.5 http://us-california-1-3.vchs.vmware.com/api/compute/api/v1.5/schema/master.xsd">
+    <Link rel="down" href="https://us-california-1-3.vchs.vmware.com/api/compute/api/org/" type="application/vnd.vmware.vcloud.orgList+xml"/>
+    <Link rel="remove" href="https://us-california-1-3.vchs.vmware.com/api/compute/api/session"/>
+    <Link rel="down" href="https://us-california-1-3.vchs.vmware.com/api/compute/api/admin/" type="application/vnd.vmware.admin.vcloud+xml"/>
+    <Link rel="down" href="https://us-california-1-3.vchs.vmware.com/api/compute/api/org/org-uuid-goes-here" name="org-name-uuid-goes-here" type="application/vnd.vmware.vcloud.org+xml"/>
+    <Link rel="down" href="https://us-california-1-3.vchs.vmware.com/api/compute/api/query" type="application/vnd.vmware.vcloud.query.queryList+xml"/>
+    <Link rel="entityResolver" href="https://us-california-1-3.vchs.vmware.com/api/compute/api/entity/" type="application/vnd.vmware.vcloud.entity+xml"/>
+    <Link rel="down:extensibility" href="https://us-california-1-3.vchs.vmware.com/api/compute/api/extensibility" type="application/vnd.vmware.vcloud.apiextensibility+xml"/>
+    <Link rel="down" href="https://us-california-1-3.vchs.vmware.com/api/compute/api/vchs/query?type=edgeGateway" type="application/vnd.vmware.vchs.query.records+xml"/>
+    <Link rel="down" href="https://us-california-1-3.vchs.vmware.com/api/compute/api/vchs/query?type=orgVdcNetwork" type="application/vnd.vmware.vchs.query.records+xml"/>
+</Session>
+`
